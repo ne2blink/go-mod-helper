4f70ed6826510021082ee42f0de9ea97f57adb2a
Need-Being <32161782+need-being@users.noreply.github.com>
2021-05-08 23:07:53 +0800

    Refactor code (#1)
    * refactor
    
    * clean up

---
+++ pkg/mod/resolve.go
@@ -0,0 +1,18 @@
+package mod
+
+import (
+	"errors"
+	"net/url"
+)
+
+// Resolve resolves a uri to go mod dependency format.
+func Resolve(uri string) (string, error) {
+	url, err := url.Parse(uri)
+	if err != nil {
+		return "", err
+	}
+	if url.Host == "github.com" {
+		return resolveGitHub(url.Path)
+	}
+	return "", errors.New("only GitHub repositories are supported")
+}
